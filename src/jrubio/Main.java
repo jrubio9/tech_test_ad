945148c0e5131a7ef1d774a8e930446a8436ae25
Jordi Rubió <rubiofeina@gmail.com>
2021-01-27 22:25:07 +0100

    Update main.java
    Included some println to split the exercises results.

--- src/jrubio/Main.java
+++ src/jrubio/Main.java
@@ -7,11 +7,25 @@ import jrubio.ex2.NumberWordsCounter;
 public class Main {
 
     public static void main(String[] args) {
-        //exercici1Test();
-        //exercici2DivideConquer();
+		System.out.println("Exercici 1:");
+		System.out.println("");
+        exercici1Test();
+        System.out.println("");
+		System.out.println("");
+		
+		System.out.println("Exercici 2:");
+		System.out.println("");
+		exercici2DivideConquer();
+		System.out.println("");
+		System.out.println("");
+		
+		System.out.println("Exercici 3:");
+		System.out.println("");
         exercici3Cutre();
         //exercici3Bo();
-
+		System.out.println("");
+		System.out.println("");
+		System.out.println("Gràcies per la vostra atenció!");
     }
 
     // Consideració: Si els elements de la llista són parells retornarà mig+1
